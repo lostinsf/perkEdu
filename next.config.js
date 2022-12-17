abe4ada88d3e9f6f9bd5f443840b62e15941cd17
이봉주 <ibongju@ibongjuui-MacBookPro.local>
2022-12-17 18:33:36 +0900

    peckEdu [enhancements] day2/221217  내부 코드 정리

--- next.config.js
+++ next.config.js
@@ -1,19 +1,18 @@
 const runtimeCaching = require("next-pwa/cache");
 const { i18n } = require('./next-i18next.config');
-const withInterceptStdout = require('next-intercept-stdout');
-const withPWA = require("next-pwa")(   withInterceptStdout(
-  {
-    i18n,
-    reactStrictMode: true,
-    pwa: {
-      dest: 'public',
-      register: true,
-      skipWaiting: true,
-      runtimeCaching,
-      disable: false,
-    },
-  },
-  (text) => (text.includes('Duplicate atom key') ? '' : text),
-),);
+const withPWA = require("next-pwa")({
 
-module.exports = withPWA;
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  runtimeCaching,
+  disable: false,
+
+});
+
+const nextConfig = withPWA({
+  reactStrictMode: true,
+  i18n,
+});
+
+module.exports = nextConfig;
